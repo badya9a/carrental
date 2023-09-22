cd6b3efe6a8de1e5af0a79b8e16a062234274ef3
Kozikov Bohdan <kozikovbohdan@Kozikovs-MacBook-Air.local>
2023-09-23 00:25:30 +0300

    changed images links

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 import { getContentType } from './api.helper'
 
 export const axiosClassic = axios.create({
-	baseURL: 'http://localhost:3000/',
+	baseURL: process.env.REACT_ADDRESS,
 	headers: getContentType(),
 })
